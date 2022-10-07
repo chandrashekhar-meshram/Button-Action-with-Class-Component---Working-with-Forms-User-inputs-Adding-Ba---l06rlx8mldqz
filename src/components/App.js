90afbc8e1345b2ab8d22cdd12c9060db39f9146e
CHANDRASHEKHAR MESHRAM <shekharonlinework@gmail.com>
2022-10-07 22:03:46 +0000

    Update package-lock.json and App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,43 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component } from "react";
+// import "../styles/App.css";
 
 class App extends Component {
-    constructor(props) {
-		super(props);
-	};
+  constructor(props) {
+    super(props);
+    this.state = {
+      show: false,
+    };
+  }
 
-    render() {
-    	return(
-    		<div id="main">
-				{ /* Do not remove this main div!! */ }
-    		</div>
-    	);
-    }
+  componentDidMount() {
+    console.log("did update run");
+    this.showFn();
+  }
+
+  showFn = () => {
+    console.log("showfn run");
+    this.setState({
+      show: this.state.show,
+    });
+  };
+
+  render() {
+    console.log("button clicked");
+    return (
+      <div id="main">
+        {/* Do not remove this main div!! */}
+        <button id="click" onClick={this.showFn}>
+          render a paragraph tag
+        </button>
+        {this.show && (
+          <p id="para">
+            Hello, I've learnt to use the full-stack evaluation tool. This
+            makesme so happy
+          </p>
+        )}
+      </div>
+    );
+  }
 }
 
-
 export default App;
-
