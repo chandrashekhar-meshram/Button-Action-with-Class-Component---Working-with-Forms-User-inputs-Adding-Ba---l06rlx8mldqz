a2be8ba0981a45ebc6f51b3aef3e63b5cf0b8a1e
CHANDRASHEKHAR MESHRAM <107882415+chandrashekhar-meshram@users.noreply.github.com>
2022-11-05 03:52:28 +0530

    Final Submition

--- src/components/App.js
+++ src/components/App.js
@@ -1,51 +1,35 @@
 import React, { Component } from "react";
-// import "../styles/App.css";
+
+import "../styles/App.css";
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      show: false,
+      show: "",
     };
   }
 
-  componentWillMount() {
-    // could do something like pull state from API
-  }
-
-  componentDidMount() {}
-
-  componentWillUnmount() {}
-
-  componentDidUpdate() {}
-
-  // componentDidMount() {
-  //   console.log("did update run");
-  //   this.showFn();
-  // }
-
-  // showFn = () => {
-  //   console.log("showfn run");
-  //   this.setState({
-  //     ...this.state,
-  //     show: true,
-  //   });
-  // };
+  showFn = () => {
+    console.log("showfn run");
+    this.setState({
+      show: `Hello, I've learnt to use the full-stack evaluation tool. This
+            makesme so happy`,
+    });
+  };
 
   render() {
     console.log("button clicked");
     return (
       <div id="main">
         {/* Do not remove this main div!! */}
-        <button id="click" onClick={this.showFn}>
+        <button
+          id="click"
+          onClick={this.showFn}
+        >
           render a paragraph tag
         </button>
-        {this.state.show && (
-          <p id="para">
-            Hello, I've learnt to use the full-stack evaluation tool. This
-            makesme so happy
-          </p>
-        )}
+        <p id="para">{this.state.show}</p>
       </div>
     );
   }
